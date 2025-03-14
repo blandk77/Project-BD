98ffba65f1bb2eb7b2c40c3459550ee1d282a9dd
blandk77 <alexags1983@gmail.com>
2025-03-14 13:22:00 +0530

    Create Headline.jsx

---
+++ frontend/src/components/Headline.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Headline() {
+  return (
+    <div className="text-center py-8">
+      <h1 className="text-white text-4xl font-bold">#No.1 Best Trusted Url Shortner</h1>
+      <p className="text-white mt-2">Earn up to 6$ / 1000 views</p>
+    </div>
+  );
+}
+
+export default Headline;
